d7730c3a2ce9e677389346041a6f7db609621b4a
Sparticus403 <twil8980@740s-pl-118.local>
2017-05-30 08:24:02 -0600

    created weapon and projectile classes

---
+++ PlatformerGame/Model/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PlatformerGame.Model
+{
+    public class Projectile
+    {
+
+        public Vector2 position;
+
+        public bool Active;
+
+        public int Damage;
+
+        Viewport viewport;
+
+        //public int Width
+        //{
+        //    get { return Texture.Width;  }
+        //}
+
+        //public int Height
+        //{
+        //    get { return Texture.Height; }
+        //}
+
+        float projectileMoveSpeed;
+
+        public Projectile()
+        {
+        }
+    }
+}
---
+++ PlatformerGame/Model/Weapon.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+
+namespace PlatformerGame.Model
+{
+    public class Weapon
+    {
+		private Texture2D texture;
+		private Vector2 origin;
+		private SoundEffect collectedSound;
+
+		public const int PointValue = 30;
+		public readonly Color Color = Color.Yellow;
+
+		// The gem is animated from a base position along the Y axis.
+		private Vector2 basePosition;
+		private float bounce;
+
+		public Level Level
+		{
+			get { return level; }
+		}
+		Level level;
+
+		/// <summary>
+		/// Gets the current position of this gem in world space.
+		/// </summary>
+		public Vector2 Position
+		{
+			get
+			{
+				return basePosition + new Vector2(0.0f, bounce);
+			}
+		}
+
+		/// <summary>
+		/// Gets a circle which bounds this gem in world space.
+		/// </summary>
+		public Circle BoundingCircle
+		{
+			get
+			{
+				return new Circle(Position, Tile.Width / 3.0f);
+			}
+		}
+
+        public Weapon()
+        {
+        }
+    }
+}
